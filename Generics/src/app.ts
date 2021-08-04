8aceea746f170ea7588986b26122a330aeaa622d
Eli <eli.i.henderson95@gmail.com>
2021-08-03 21:44:32 -0400

    Generic Utility Types

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -107,5 +107,26 @@ objStorage.removeItem(maxObj)
 console.log(objStorage.getItems())
 
 //Generic Utility Types 
+//These types only exist it typescript 
 
+interface CourseGoal {
+    title: string; 
+    description: string;
+    completeUntil: Date; 
+}
 
+// function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+//     return {title: title, description: description, completeUntil:date}
+// }
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+        let courseGoal: Partial<CourseGoal> = {};
+        courseGoal.title = title; 
+        courseGoal.description = description;
+        courseGoal.completeUntil = date; 
+
+        return courseGoal as CourseGoal; 
+}
+    
+
+// const names: Readonly<string[]> = ['Max', 'Anna']
